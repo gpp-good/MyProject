5849cde15c86a2442e1448e12631704a1040ffd6
gpp-good <cj1798838076@outlook.com>
2019-01-08 22:26:32 +0800

    first commit

---
+++ move_function/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,57 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+class IntNum
+{
+public:
+	IntNum(int x = 0);
+	IntNum(const IntNum & n);
+	IntNum(IntNum && n);
+	~IntNum();
+	int getInt()
+	{
+		return *xptr;
+	}
+private:
+	int * xptr;
+};
+
+IntNum::IntNum(int x)
+{
+	xptr = new int(x);
+	std::cout << "Caling constructor..." << std::endl;
+}
+
+IntNum::IntNum(const IntNum & n)
+{
+	xptr = new int(*n.xptr);
+	std::cout << "Caling conpy constructor..." << std::endl;
+}
+
+IntNum::IntNum(IntNum && n)
+{
+	xptr = n.xptr;
+	n.xptr = nullptr;
+	std::cout << "Caling move constructor..." << std::endl;
+}
+
+IntNum::~IntNum()
+{
+	delete xptr;
+	std::cout << "Delete..." << std::endl;
+
+}
+IntNum getNum()
+{
+	IntNum a;
+	return a;
+}
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	std::cout << getNum().getInt() << std::endl;
+
+}
+
+
